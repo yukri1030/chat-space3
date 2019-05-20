de652cc5615f2c3f35c7ea135299198021e1abe3
yukri1030 <sky.moon.go@gmail.com>
2019-05-20 11:26:03 +0900

    再度、一番最初に設定していたルートの記述に直し、実行
    root "messages#index"
    のみ設定。実行した。
    
    今度は、@import module読み込まれないと言われてしまった

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to :'devise/session#new'
-  get 'message/index' => "messages#index"
+  root "messages#index"
 end
