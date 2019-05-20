3d75d0b2ac1b599bc15045af87c814b181c985a4
yukri1030 <sky.moon.go@gmail.com>
2019-05-20 11:12:47 +0900

    ルートのコード変更するもエラーになった
     root to :'devise/session#new'
      get 'user/sign_in' to: 'messages#index'
    end
    
    と書き換え
    
    2行目、get~~がおかしいとエラーになってしまう

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to :'devise/session#new'
-  get 'message/index' => "messages#index"
+  get 'user/sign_in' to: 'messages#index'
 end
