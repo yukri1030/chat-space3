7b57ecf1b5cda233fc7776f0e2e048225cebb2f2
yukri1030 <sky.moon.go@gmail.com>
2019-05-20 10:56:44 +0900

    ルートの書き換え（書き方を変えてみました）
    ルートに以下のように定義
    
    devise_for :users
      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
      root to :'devise/session#new'
      get 'message/index' => "messages#index"
    
    エラー変わらず。devise_fo :usersがないと言われてしまいました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "devise/session#new"
+  root to :'devise/session#new'
   get 'message/index' => "messages#index"
 end
