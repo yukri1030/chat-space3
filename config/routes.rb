7f8f5db41ca302d3195896657019c512287ea307
yukri1030 <sky.moon.go@gmail.com>
2019-05-22 20:48:44 +0900

    スペルミス（endがない）のため、追記

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+  end
+end
