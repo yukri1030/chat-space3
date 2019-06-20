1aa4ae37bda3689a452ed8a8993929d1d7d00042
yukri1030 <sky.moon.go@gmail.com>
2019-06-20 12:18:34 +0900

    スペルミスを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.11.0'
 
 set :application, 'chat-space3'
-set :repo_url,  'git@github.com:yukiri1030/chat-space3.git'
+set :repo_url,  'git@github.com:yukri1030/chat-space3.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
