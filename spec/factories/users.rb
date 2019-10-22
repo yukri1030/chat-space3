86ea2250c93b7f63c611272593c1a7f28188ffe9
abe yukari <sakura@AbenoMacBook-Air.local>
2019-10-22 14:39:17 +0900

    エラーのため修正
    # WHTA
    エラー文
    https://i.gyazo.com/b09765c156ff186254cf31b0c3fb8b42.png
    
    # WHY
    gactoribot　のバリデーションがかかっていないことが原因

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    password = Faker::Internet.password(8)
+    password = Faker::Internet.password(min_length: 8)
     name {Faker::Name.last_name}
     email {Faker::Internet.free_email}
     password {password}
